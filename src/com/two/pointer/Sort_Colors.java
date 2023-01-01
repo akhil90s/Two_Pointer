6dfa5a6ac740a27321a9595fe8b540988c92f2e3
akhil90s <46933461+akhil90s@users.noreply.github.com>
2023-01-01 14:14:50 +0530

    Two_Pointer

---
+++ src/com/two/pointer/Sort_Colors.java
@@ -0,0 +1,36 @@
+package com.two.pointer;
+
+public class Sort_Colors {
+
+	public static void main(String[] args) {
+
+		int[] array = new int[] { 1, 0, 2, 1, 1, 0 };
+		sortColors_Approach1(array);
+
+	}
+
+	// Dutch National Flag Problem
+	// https://leetcode.com/problems/sort-colors/
+	public static void sortColors_Approach1(int[] nums) {
+		
+		int start = 0;
+		int end = nums.length - 1;
+		int index = 0;
+		while (index <= end) {
+			if (nums[index] == 0) {
+				nums[index] = nums[start];
+				nums[start] = 0;
+				index++;
+				start++;
+			} else if (nums[index] == 2) {
+				nums[index] = nums[end];
+				nums[end] = 2;
+				end--;
+			} else {
+				index++;
+			}
+		}
+		return;
+	}
+
+}
